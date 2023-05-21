5fe1e3356bb3f4922820aa68d65906b79cc140e8
roxana-o <120491557+roxana-o@users.noreply.github.com>
2023-05-21 22:51:35 +0300

    Add files via upload

---
+++ FormPay/FIS/FormPay.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace FIS
+{
+    partial class FormPay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.lblCash = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblCardName = new System.Windows.Forms.Label();
+            this.txtCardName = new System.Windows.Forms.TextBox();
+            this.txtCardNumber = new System.Windows.Forms.TextBox();
+            this.lblCardNumber = new System.Windows.Forms.Label();
+            this.txtCVV = new System.Windows.Forms.TextBox();
+            this.lblCVV = new System.Windows.Forms.Label();
+            this.txtExpirationDate = new System.Windows.Forms.TextBox();
+            this.lblExpirationDate = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(357, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Modalitatea de plată";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(221, 131);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(80, 20);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Numerar";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(221, 187);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(102, 20);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Card bancar";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // lblCash
+            // 
+            this.lblCash.AutoSize = true;
+            this.lblCash.Location = new System.Drawing.Point(379, 132);
+            this.lblCash.Name = "lblCash";
+            this.lblCash.Size = new System.Drawing.Size(216, 48);
+            this.lblCash.TabIndex = 3;
+            this.lblCash.Text = "* Puteți alege modalitatea de plată,\r\nfie în numerar la livrare, fie acum, cu\r\nca" +
+    "rdul bancar. \r\n";
+            this.lblCash.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(382, 353);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 27);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Confirmă";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblCardName
+            // 
+            this.lblCardName.AutoSize = true;
+            this.lblCardName.Location = new System.Drawing.Point(357, 97);
+            this.lblCardName.Name = "lblCardName";
+            this.lblCardName.Size = new System.Drawing.Size(98, 32);
+            this.lblCardName.TabIndex = 7;
+            this.lblCardName.Text = "Nume deținător\r\n(Ion Popescu)";
+            // 
+            // txtCardName
+            // 
+            this.txtCardName.Location = new System.Drawing.Point(519, 107);
+            this.txtCardName.Name = "txtCardName";
+            this.txtCardName.Size = new System.Drawing.Size(155, 22);
+            this.txtCardName.TabIndex = 8;
+            // 
+            // txtCardNumber
+            // 
+            this.txtCardNumber.Location = new System.Drawing.Point(519, 135);
+            this.txtCardNumber.Name = "txtCardNumber";
+            this.txtCardNumber.Size = new System.Drawing.Size(155, 22);
+            this.txtCardNumber.TabIndex = 10;
+            // 
+            // lblCardNumber
+            // 
+            this.lblCardNumber.AutoSize = true;
+            this.lblCardNumber.Location = new System.Drawing.Point(357, 135);
+            this.lblCardNumber.Name = "lblCardNumber";
+            this.lblCardNumber.Size = new System.Drawing.Size(139, 32);
+            this.lblCardNumber.TabIndex = 9;
+            this.lblCardNumber.Text = "Număr card\r\n(1298-1234-5678-9012)";
+            // 
+            // txtCVV
+            // 
+            this.txtCVV.Location = new System.Drawing.Point(519, 175);
+            this.txtCVV.Name = "txtCVV";
+            this.txtCVV.Size = new System.Drawing.Size(155, 22);
+            this.txtCVV.TabIndex = 12;
+            // 
+            // lblCVV
+            // 
+            this.lblCVV.AutoSize = true;
+            this.lblCVV.Location = new System.Drawing.Point(357, 175);
+            this.lblCVV.Name = "lblCVV";
+            this.lblCVV.Size = new System.Drawing.Size(72, 32);
+            this.lblCVV.TabIndex = 11;
+            this.lblCVV.Text = "CVV/CVV2\r\n(990)";
+            // 
+            // txtExpirationDate
+            // 
+            this.txtExpirationDate.Location = new System.Drawing.Point(519, 217);
+            this.txtExpirationDate.Name = "txtExpirationDate";
+            this.txtExpirationDate.Size = new System.Drawing.Size(155, 22);
+            this.txtExpirationDate.TabIndex = 14;
+            // 
+            // lblExpirationDate
+            // 
+            this.lblExpirationDate.AutoSize = true;
+            this.lblExpirationDate.Location = new System.Drawing.Point(357, 215);
+            this.lblExpirationDate.Name = "lblExpirationDate";
+            this.lblExpirationDate.Size = new System.Drawing.Size(86, 32);
+            this.lblExpirationDate.TabIndex = 13;
+            this.lblExpirationDate.Text = "Data expirării\r\n(11/2024)";
+            // 
+            // FormPay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtExpirationDate);
+            this.Controls.Add(this.lblExpirationDate);
+            this.Controls.Add(this.txtCVV);
+            this.Controls.Add(this.lblCVV);
+            this.Controls.Add(this.txtCardNumber);
+            this.Controls.Add(this.lblCardNumber);
+            this.Controls.Add(this.txtCardName);
+            this.Controls.Add(this.lblCardName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblCash);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Name = "FormPay";
+            this.Text = "Plata";
+            this.Load += new System.EventHandler(this.FormPay_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label lblCash;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblCardName;
+        private System.Windows.Forms.TextBox txtCardName;
+        private System.Windows.Forms.TextBox txtCardNumber;
+        private System.Windows.Forms.Label lblCardNumber;
+        private System.Windows.Forms.TextBox txtCVV;
+        private System.Windows.Forms.Label lblCVV;
+        private System.Windows.Forms.TextBox txtExpirationDate;
+        private System.Windows.Forms.Label lblExpirationDate;
+    }
+}
+
