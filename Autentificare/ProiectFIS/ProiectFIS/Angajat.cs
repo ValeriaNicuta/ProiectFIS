70df83cc0ec190f222540cc19ddc90d147fb3304
Valeria <115549956+ValeriaNicuta@users.noreply.github.com>
2023-05-22 01:25:40 +0300

    Forme autentificare

---
+++ Autentificare/ProiectFIS/ProiectFIS/Angajat.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProiectFIS
+{
+    public partial class Angajat : Form
+    {
+        public Angajat()
+        {
+            InitializeComponent();
+        }
+
+        private int contor = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                MessageBox.Show("Introduceti date valide", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                goto exitLabel;
+            }
+
+            string[] utilizatori = File.ReadAllLines("angajat.txt");
+            foreach (var line in utilizatori)
+            {
+                string[] inregistrare = line.Split(',');
+                if ((textBox1.Text).Equals(inregistrare[0]))
+                {
+                    if ((textBox2.Text.Trim()).Equals(inregistrare[1].Trim()))
+                    {
+                        this.Hide();
+                        Form f = new Pornire(); //se va lega forma pentru acces pagina angajatilor
+                        f.ShowDialog();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Parola incorecta!");
+                    }
+                    contor = 1;
+                }
+            }
+
+            if (contor == 0)
+                MessageBox.Show("Nu exista cont inregistrat cu acest user si parola", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            exitLabel:;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form f = new Pornire(); //in caz ca s-a ales optiunea gresita
+            f.ShowDialog();
+        }
+    }
+}
---
+++ Autentificare/ProiectFIS/ProiectFIS/Inregistrare.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProiectFIS
+{
+    public partial class Inregistrare : Form
+    {
+        public Inregistrare()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (textBox1.Text == "" || textBox2.Text == ""|| textBox3.Text == ""|| textBox4.Text == "")
+            {
+                MessageBox.Show("Introduceti date valide", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            using (StreamWriter fout = File.AppendText("utilizatori.txt"))
+            {
+                if ((textBox3.Text).Equals(textBox4.Text))
+                {
+                    fout.WriteLine(textBox1.Text.Trim() + "," + textBox3.Text.Trim());
+                    MessageBox.Show("Creare cont reusita!");
+                    this.Hide();
+                    Form f = new Client();
+                    f.ShowDialog();
+                }
+                else
+                {
+                    MessageBox.Show("Parola nu corespunde", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                fout.Close();
+            }
+           
+        }
+    }
+}
