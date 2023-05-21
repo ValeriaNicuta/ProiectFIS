cd363a8caeb4e200ff6faff6ca770d00993f051b
Nistor Cristina <93156181+cristinanistor1@users.noreply.github.com>
2023-05-21 20:03:18 +0300

    User
    user : user
    password : password

---
+++ ProiectFis/ProiectFis/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProiectFis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if ((textBox1.Text == "user") && (textBox2.Text == "password"))
+            {
+                UserDepunereColet f = new UserDepunereColet();
+                this.Hide();
+                f.ShowDialog();
+            }
+            else
+                MessageBox.Show("Utilizator/Parola incorecte!");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
